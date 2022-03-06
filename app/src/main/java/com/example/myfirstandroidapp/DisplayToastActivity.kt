09dc48eb8a2f3220193aa7952deca7c61c9b7c9b
IgorKa <igor.karlik2973@gmail.com>
2022-03-06 09:53:58 +0100

    Added ImageView Toast
    ImageView displaying Toast message added to "Toast" side page.

--- app/src/main/java/com/example/myfirstandroidapp/DisplayToastActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/DisplayToastActivity.kt
@@ -3,6 +3,7 @@ package com.example.myfirstandroidapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class DisplayToastActivity : AppCompatActivity() {
@@ -10,10 +11,16 @@ class DisplayToastActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_toast)
 
+        val imageViewToast = findViewById(R.id.image_view) as ImageView
+        imageViewToast.setOnClickListener {
+            // make a toast on button click event
+            Toast.makeText(this, "Hi there! You've clicked imageView Toaster!", Toast.LENGTH_SHORT).show()
+        }
+
         val shortToast = findViewById(R.id.short_toast) as Button
         shortToast.setOnClickListener {
             // make a toast on button click event
-            Toast.makeText(this, "Hi ther! This toast displays shorter.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Hi there! This toast displays shorter.", Toast.LENGTH_SHORT).show()
         }
 
         val longToast = findViewById(R.id.long_toast) as Button
